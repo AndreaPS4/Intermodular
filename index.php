a376a69d38034a08fc30259a150f418db0890429
AndreaP131 <andrea.pradas.andrea@gmail.com>
2025-03-15 17:39:13 +0100

    Trabajo en el header

--- index.php
+++ index.php
@@ -6,6 +6,30 @@
     <title>Chimichurri Cómics</title>
 </head>
 <body>
-    
+<header>
+        <div class="header-container">
+            <div class="logo">
+                <img src="img/logo.png" class="icono" alt="Logo de Chimichurri Cómics">
+                <img src="img/eslogan.png" class="eslogan" alt="Eslogan Chimichurri Cómics">
+            </div>
+            <div class="search-container">
+                <form action="resultados.php" method="GET">
+                    <input type="text" class="search-box" name="query" placeholder="Buscar..." id="searchInput" required>
+                    <button type="submit" class="search-button"></button>
+                </form>    
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="#">Novedades</a></li>
+                    <li><a href="#">Próximamente</a></li>
+                    <li><a href="catalogo.php">Catálogo</a></li>
+                    <li><a href="registrarse.php">Regístrate</a></li>
+                    <li><a href="login.php">Inicia Sesión</a></li>
+                    <li><a href="carrito.html">Carrito</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 </body>
 </html>
